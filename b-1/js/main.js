02211d15f12cc00f279d643bea0a2eefc8ac27d2
doheeeeeeeeeee <lhamster@naver.com>
2024-05-01 17:19:39 +0900

    0501-3

--- b-1/js/main.js
+++ b-1/js/main.js
@@ -1,42 +1,46 @@
 
 $(function(){
-    //gnb
-    $(".gnb").hover(){
-        function(){
-            $ ("gnb ul"),stop(), slidedown()
-        }
-        function(){
-            $(".gnb ul").stop().slideup
-        }
-        }
-
-    }
+        //gnb
+        $(".gnb").hover(
+            function(){
+                $ (".gnb ul").stop() .slidedown()
+            },
+            function(){
+                $(".gnb ul").stop() .slideup()
+            }
+            }
+        )
+               
+            
 
         //slide
-        setInterval(function(){},3000)
-            $(".slides").animate({top : "-100"}, function(){
-                $(this),append($(".slide").eq(0))
+        setInterval(function(){
+            $(".slides").animate({top : "-100%"}, function(){
+                $(this),append($(" .slide").eq(0))
                 $(this).css("top",0)
             })
 
+        },3000)
+           
+
         //tab
-        $("tab_tile h4").click(function(){
+        $(".tab_tile h4").click(function(){
             $(".tab_title h4").removeClass("active")
             $(this).addClass("active")
-            var
-            $ ("tab_contents ul").hide(){
+            
+            var idx = $(this).index()
 
-            }
-            $("tab_contents ul").
+            $ ("tab_contents ul").hide()
+            $("tab_contents ul").eq(idx) .show()
         })
 
         //modal
-        $("showModal").click(function(){
-            $(".modal").
+        $("#showModal").click(function(){
+            $(".modal").show()
         })
 
-        $("showModal").click(function(){
-            $(".modal").
+        $("#closeModal").click(function(){
+            $(".modal").hide()
         })
 
-})
+} )
